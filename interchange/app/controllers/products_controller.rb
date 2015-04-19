d9c71188e9b633daee877b74ff1f3b960fa3d5ab
dabe20 <d.a.b.e_20@hotmail.com>
2015-04-19 12:25:38 -0600

    commit 06
    se modifico tabla Product, se creo la vista show e index para products

--- interchange/app/controllers/products_controller.rb
+++ interchange/app/controllers/products_controller.rb
@@ -1,4 +1,11 @@
 class ProductsController < ApplicationController
-  def index
-  end
+  
+  #GET  /products
+	def index
+		@products = Product.all
+	end
+	#GET /products/:id
+	def show
+		@products = Product.find(params[:id])
+	end
 end
--- interchange/db/schema.rb
+++ interchange/db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415020320) do
+ActiveRecord::Schema.define(version: 20150419174616) do
 
   create_table "products", force: true do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string   "name"
+    t.text     "description"
+    t.text     "state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
