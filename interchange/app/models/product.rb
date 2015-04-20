bd6f5b148a7d390ccea34ef8bf2f076d90789279
dabe20 <d.a.b.e_20@hotmail.com>
2015-04-20 04:51:46 -0600

    commit 13
    se hicieron asociaciones para los usuarios y productos, y ademas se creo
    una migracion a la BD

--- interchange/app/models/product.rb
+++ interchange/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	belongs_to :user
 	validates :name, presence: true, length: {minimum: 6}, uniqueness: true
 	validates :description, presence: true, length: {minimum: 6}
 	validates :state, presence: true, length: {minimum: 6}
